3c8c2004e466e55311824026ecf16bce0865113c
unknown <yuray8901@gmail.com>
2021-12-07 16:54:44 +0800

    origin

---
+++ MRT_Food_Map/station_restaurant.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Left Sidebar - Strongly Typed by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="left-sidebar is-preload">
+		<div id="page-wrapper">
+			<!-- Header -->
+				<section id="header">
+					<div class="container">
+
+						<!-- Logo -->
+							<h1 id="logo" ><a href="index.html">中山站</a></h1>
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li><a class="icon solid fa-home" href="index.html"><span>Introduction</span></a></li>
+									<li>
+										<a href="#" class="icon fa-chart-bar"><span>Dropdown</span></a>
+										<ul>
+											<li><a href="#">Lorem ipsum dolor</a></li>
+											<li><a href="#">Magna phasellus</a></li>
+											<li><a href="#">Etiam dolore nisl</a></li>
+											<li>
+												<a href="#">Phasellus consequat</a>
+												<ul>
+													<li><a href="#">Magna phasellus</a></li>
+													<li><a href="#">Etiam dolore nisl</a></li>
+													<li><a href="#">Phasellus consequat</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Veroeros feugiat</a></li>
+										</ul>
+									</li>
+									<li><a class="icon solid fa-cog" href="left-sidebar.html"><span>Left Sidebar</span></a></li>
+									<li><a class="icon solid fa-retweet" href="right-sidebar.html"><span>Right Sidebar</span></a></li>
+									<li><a class="icon solid fa-sitemap" href="no-sidebar.html"><span>No Sidebar</span></a></li>
+								</ul>
+							</nav>
+
+					</div>
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+				
+
+							<!-- Sidebar -->
+								<div id="sidebar" class="col-4 col-12-medium">
+
+									<!-- Excerpts -->
+										<section>
+											<ul class="divided">
+												<li>
+													<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">鬼金棒</span>
+																<h3>地址:</h3>
+																<h3>電話:</h3>
+																<h3>菜單:</h3>
+															</header>
+
+															<!--php-->
+									
+															<p><a class="icon solid fa-heart" ><span>讚</span></a>
+															&emsp;
+															<a class="icon solid fa-plus" ><span>收藏</span></a></p>
+
+														</article>
+												</li>
+												<li>
+													<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">鬼金棒</span>
+																<h3>地址:</h3>
+																<h3>電話:</h3>
+																<h3>菜單:</h3>
+															</header>
+															<!--php-->
+															<p><a class="icon solid fa-heart" ><span>讚</span></a>
+															&emsp;
+															<a class="icon solid fa-plus" ><span>收藏</span></a></p>
+														</article>
+												</li>
+												<li>
+													<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">鬼金棒</span>
+																<h3>地址:</h3>
+																<h3>電話:</h3>
+																<h3>菜單:</h3>
+															</header>
+															<!--php-->
+															<p><a class="icon solid fa-heart" ><span>讚</span></a>
+															&emsp;
+															<a class="icon solid fa-plus" ><span>收藏</span></a></p>
+														</article>
+												</li>
+											</ul>
+										</section>
+								</div>
+						</div>
+					</div>
+				</section>
+
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
